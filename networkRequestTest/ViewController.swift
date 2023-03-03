2f754e525123f1ef299c3224eb2847896fa6887e
KalYvesGit <kalivayves>
2023-03-03 18:30:55 +0300

    Initial Commit

---
+++ networkRequestTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  networkRequestTest
+//
+//  Created by Jean Yves Kaliva on 3/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
