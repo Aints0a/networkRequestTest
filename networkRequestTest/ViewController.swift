5cd43ad58cd797cc4120d2c91fce7327df0549ac
KalYvesGit <kalivayves>
2023-03-04 07:16:53 +0300

    Add image view, creat an URLimage object, create an ULRSession object, give task to URLSession, add button to load the image.

--- networkRequestTest/ViewController.swift
+++ networkRequestTest/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+   
     }
-
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("Button pressed.")
+        let imageUrl = URL(string: "https://www.shutterstock.com/image-photo/beautiful-rottweiler-dog-park-leash-260nw-696851572.jpg")!
+        let task = URLSession.shared.dataTask(with: imageUrl) { data, response, error in
+            if let error = error {
+                print(error)
+                
+            } else {
+                if let data = data, let image = UIImage(data: data){
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    print("task executed")
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
 }
 
